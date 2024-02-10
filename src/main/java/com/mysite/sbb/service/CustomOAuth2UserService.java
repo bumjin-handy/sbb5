0f7e6302a71bf6507b4d12b67be1be1b29217f9d
bumjin <bumjin@gmail.com>
2024-02-10 21:23:14 +0900

    OAuth 로그인 기능추가 #56
    OAuth 로그인 후 유저정보 테이블에 저장

--- src/main/java/com/mysite/sbb/service/CustomOAuth2UserService.java
+++ src/main/java/com/mysite/sbb/service/CustomOAuth2UserService.java
@@ -4,15 +4,28 @@ import com.mysite.sbb.dto.CustomOAuth2User;
 import com.mysite.sbb.dto.GoogleReponse;
 import com.mysite.sbb.dto.NaverResponse;
 import com.mysite.sbb.dto.OAuth2Response;
+import com.mysite.sbb.user.SiteUser;
+import com.mysite.sbb.user.UserRepository;
+import com.mysite.sbb.user.UserRole;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     //DefaultOAuth2UserService OAuth2UserService의 구현체
+
+    private final UserRepository userRepository;
+
+    public CustomOAuth2UserService(UserRepository userRepository) {
+
+        this.userRepository = userRepository;
+    }
+
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -35,7 +48,29 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
             return null;
         }
 
-        String role = "ROLE_USER";
+        String username = oAuth2Response.getProvider()+" "+oAuth2Response.getProviderId();
+        Optional<SiteUser> _siteUser = userRepository.findByUsername(username);
+
+        String role = null;
+        if (_siteUser.isEmpty()) {
+
+            SiteUser userEntity = new SiteUser();
+            userEntity.setUsername(username);
+            userEntity.setEmail(oAuth2Response.getEmail());
+            userEntity.setRole(UserRole.USER.getValue());
+
+            userRepository.save(userEntity);
+        }
+        else {
+            SiteUser userEntity = _siteUser.get();
+            userEntity.setUsername(username);
+            userEntity.setEmail(oAuth2Response.getEmail());
+
+            role = _siteUser.get().getRole();
+
+            userRepository.save(_siteUser.get());
+        }
+
         return new CustomOAuth2User(oAuth2Response, role);
     }
 }
