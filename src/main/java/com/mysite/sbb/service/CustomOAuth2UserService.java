80083bb75aeb3528eaf68b9c7eff88716cbd1da7
bumjin <bumjin@gmail.com>
2024-02-10 17:11:31 +0900

    OAuth 로그인 기능추가 #56
    naver와 google 로그인 기능 추가
    네이버 개발자센터와 구글GCP 에서 소셜로그인 인증 Key 발급 필요.

---
+++ src/main/java/com/mysite/sbb/service/CustomOAuth2UserService.java
@@ -0,0 +1,41 @@
+package com.mysite.sbb.service;
+
+import com.mysite.sbb.dto.CustomOAuth2User;
+import com.mysite.sbb.dto.GoogleReponse;
+import com.mysite.sbb.dto.NaverResponse;
+import com.mysite.sbb.dto.OAuth2Response;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+    //DefaultOAuth2UserService OAuth2UserService의 구현체
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        System.out.println(oAuth2User.getAttributes());
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        OAuth2Response oAuth2Response = null;
+        if (registrationId.equals("naver")) {
+
+            oAuth2Response = new NaverResponse(oAuth2User.getAttributes());
+        }
+        else if (registrationId.equals("google")) {
+
+            oAuth2Response = new GoogleReponse(oAuth2User.getAttributes());
+        }
+        else {
+
+            return null;
+        }
+
+        String role = "ROLE_USER";
+        return new CustomOAuth2User(oAuth2Response, role);
+    }
+}
